768e238742bed22a85134c2b21d0b9abb61db6d0
atsushi watanabe <watanabe@sunrisedigital.jp>
2017-03-17 19:16:22 +0900

    DB接続

--- web/index.aspx
+++ web/index.aspx
@@ -5,13 +5,19 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head runat="server">
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
-    <title></title>
+  <title></title>
 </head>
 <body>
-    <form id="form1" runat="server">
-    <div>
+  <form id="form1" runat="server">
+  <div>
+    <% 
+       foreach (System.Data.DataRow drow in dt.Rows)
+       {
+         Response.Write(drow["name"] + "<br>");
+       }
     
-    </div>
-    </form>
+     %>
+  </div>
+  </form>
 </body>
 </html>
--- web/index.aspx.cs
+++ web/index.aspx.cs
@@ -4,11 +4,57 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.IO;
+using System.Text;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
 
 public partial class index : System.Web.UI.Page
 {
-    protected void Page_Load(object sender, EventArgs e)
-    {
 
+  public DataTable dt;
+
+  public DataTable GetData()
+  {
+    var table = new DataTable();
+
+    var connectionString = ConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString;
+
+    using (var conn = new SqlConnection(connectionString))
+    using (var command = conn.CreateCommand())
+    {
+      try
+      {
+        // データベースの接続開始
+        conn.Open();
+
+        // SQLの設定
+        command.CommandText = @"SELECT * FROM Staff";
+
+        // SQLの実行
+        var adapter = new SqlDataAdapter(command);
+        adapter.Fill(table);
+      }
+      catch (Exception exception)
+      {
+        Console.WriteLine(exception.Message);
+        throw;
+      }
+      finally
+      {
+        // データベースの接続終了
+        conn.Close();
+      }
     }
+    return table;
+  }
+
+
+  protected void Page_Load(object sender, EventArgs e)
+  {
+    dt = GetData();
+
+
+  }
 }
