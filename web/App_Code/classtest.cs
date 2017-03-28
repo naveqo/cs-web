9c553c2392f36c3105bf4bce425da3bb1c4d5816
atsushi watanabe <watanabe@sunrisedigital.jp>
2017-03-28 18:59:48 +0900

    クラスのテスト中

---
+++ web/App_Code/classtest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// classtest の概要の説明です
+/// </summary>
+public class Complex
+{
+  public double re; // 実部を記憶しておく(外部からの読み出し・書き換えも可能)
+  public double im; // 虚部を記憶しておく(外部からの読み出し・書き換えも可能)
+
+  // 絶対値を取り出す
+  public double Abs()
+  {
+    return Math.Sqrt(re * re + im * im);// Math.Sqrt は平方根を求める関数
+  }
+
+  public override string ToString()
+  {
+    if(im > 0)
+      return re.ToString() + "+i" + im.ToString();
+    if(im < 0)
+      return re.ToString() + "-i" + (-im).ToString();
+    return re.ToString();
+  }
+}
---
+++ web/test.aspx.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class test : System.Web.UI.Page
+{
+
+  public Complex z = new Complex();
+
+  protected void Page_Load(object sender, EventArgs e)
+  {
+    z.re = 3;
+    z.im = 4;
+    double abs = z.Abs();
+    string aaa = z.ToString();
+
+    Response.Write(abs + "<br>");
+    Response.Write(aaa);
+  }
+}
