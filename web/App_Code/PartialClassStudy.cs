2dccc62ed4e7e6738df6fe64327ab8ee7e947efe
atsushi watanabe <watanabe@sunrisedigital.jp>
2017-03-31 15:52:10 +0900

    partialクラスのテスト

---
+++ web/App_Code/PartialClassStudy.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// PartialClassStudy の概要の説明です
+/// </summary>
+partial class Program
+{
+  static void Main(string[] arts)
+  {
+    OnBeginProgram();
+
+    Console.Write("program body\n");
+
+    OnEndProgram();
+  }
+
+  static partial void OnBeginProgram();
+  static partial void OnEndProgram();
+}
