717e55379dcb68e73892c2d6a4ae067c79e98508
atsushi watanabe <watanabe@sunrisedigital.jp>
2017-03-31 13:52:42 +0900

    構造体サンプル

---
+++ web/structure.aspx.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class structure : System.Web.UI.Page
+{
+
+  struct Sample
+  {
+    public int _x;
+    public int _y;
+
+    public Sample(int x, int y)
+    {
+      _x = x;
+      _y = y;
+      M();
+    }
+
+    void M() { }
+  }
+
+
+  protected void Page_Load(object sender, EventArgs e)
+  {
+    Sample s = new Sample();
+    Response.Write(s._x);
+  }
+}
